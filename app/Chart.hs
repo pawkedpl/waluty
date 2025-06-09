a2cecc82b21335a83790927187407ebc47ec1195
wyz3r0 <dobroszek.d@gmail.com>
2025-06-09 21:59:24 +0200

    fixed multople currencies plot and name format

--- app/Chart.hs
+++ app/Chart.hs
@@ -20,101 +20,105 @@ import System.Directory (createDirectoryIfMissing)
 import System.FilePath ((</>))
 
 data Rate = Rate
-  { currency :: String
-  , code :: String
-  , mid :: Double
-  } deriving Show
+    { currency :: String
+    , code :: String
+    , mid :: Double
+    } deriving Show
 
 instance FromJSON Rate where
-  parseJSON = withObject "Rate" $ \v ->
-    Rate <$> v .: fromString "currency"
-         <*> v .: fromString "code"
-         <*> v .: fromString "mid"
+    parseJSON = withObject "Rate" $ \v ->
+        Rate <$> v .: fromString "currency"
+             <*> v .: fromString "code"
+             <*> v .: fromString "mid"
 
 data RateTable = RateTable
-  { effectiveDate :: String
-  , rates :: [Rate]
-  } deriving Show
+    { effectiveDate :: String
+    , rates :: [Rate]
+    } deriving Show
 
 instance FromJSON RateTable where
-  parseJSON = withObject "RateTable" $ \v ->
-    RateTable <$> v .: fromString "effectiveDate"
-              <*> v .: fromString "rates"
+    parseJSON = withObject "RateTable" $ \v ->
+        RateTable <$> v .: fromString "effectiveDate"
+                  <*> v .: fromString "rates"
 
 type RateTables = [RateTable]
 
 fetchRatesInRange :: Manager -> String -> Day -> Day -> IO [(Day, Double)]
 fetchRatesInRange manager curr startDay endDay = do
-  let
-    startStr = formatTime defaultTimeLocale "%Y-%m-%d" startDay
-    endStr = formatTime defaultTimeLocale "%Y-%m-%d" endDay
-    url = "https://api.nbp.pl/api/exchangerates/tables/A/" ++ startStr ++ "/" ++ endStr ++ "?format=json"
+    let
+        startStr = formatTime defaultTimeLocale "%Y-%m-%d" startDay
+        endStr = formatTime defaultTimeLocale "%Y-%m-%d" endDay
+        url = "https://api.nbp.pl/api/exchangerates/tables/A/" ++ startStr ++ "/" ++ endStr ++ "?format=json"
 
-    handler :: HttpException -> IO [(Day, Double)]
-    handler _ = return []
+        handler :: HttpException -> IO [(Day, Double)]
+        handler _ = return []
 
-  (do
-      req <- parseRequest url
-      resp <- httpLbs req manager
-      let body = responseBody resp
-      case eitherDecode body :: Either String RateTables of
-        Right tables -> do
-          let pairs =
-                [ (dayFromString (effectiveDate t), findMid curr (rates t))
-                | t <- tables
-                , currencyExists curr (rates t)
-                ]
-          return pairs
-        Left err -> do
-          putStrLn $ "Błąd dekodowania JSON: " ++ err
-          return []
-    )
-    `catch` handler
+    (do
+        req <- parseRequest url
+        resp <- httpLbs req manager
+        let body = responseBody resp
+        case eitherDecode body :: Either String RateTables of
+            Right tables -> do
+                let pairs =
+                        [ (dayFromString (effectiveDate t), findMid curr (rates t))
+                        | t <- tables
+                        , currencyExists curr (rates t)
+                        ]
+                return pairs
+            Left err -> do
+                putStrLn $ "Błąd dekodowania JSON: " ++ err
+                return []
+        )
+        `catch` handler
 
 dayFromString :: String -> Day
 dayFromString s =
-  case parseTimeM True defaultTimeLocale "%Y-%m-%d" s of
-    Just d -> d
-    Nothing -> error $ "Niepoprawna data: " ++ s
+    case parseTimeM True defaultTimeLocale "%Y-%m-%d" s of
+        Just d -> d
+        Nothing -> error $ "Niepoprawna data: " ++ s
 
 currencyExists :: String -> [Rate] -> Bool
 currencyExists c = any (\r -> map toUpper (code r) == map toUpper c)
 
 findMid :: String -> [Rate] -> Double
 findMid c rs =
-  case filter (\r -> map toUpper (code r) == map toUpper c) rs of
-    (r:_) -> mid r
-    [] -> error $ "Brak kursu dla waluty: " ++ c
+    case filter (\r -> map toUpper (code r) == map toUpper c) rs of
+        (r:_) -> mid r
+        [] -> error $ "Brak kursu dla waluty: " ++ c
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    [curr] -> do
-      manager <- newManager tlsManagerSettings
-      today <- utctDay <$> getCurrentTime
-      let startDay = addDays (-30) today
+    args <- getArgs
+    case args of
+        [] -> putStrLn "Podaj co najmniej jedną walutę jako argument."
+        currs -> do
+            manager <- newManager tlsManagerSettings
+            now <- getCurrentTime
+            let today = utctDay now
+                startDay = addDays (-30) today
+                timestamp = formatTime defaultTimeLocale "%Y-%m-%d_%H-%M-%S" now
+                currStr = List.intercalate "_" currs
+                fileName = "plots" </> (timestamp ++ "_" ++ currStr ++ ".svg")
 
-      putStrLn $ "Pobieram kursy " ++ curr ++ " z ostatnich 30 dni..."
+            putStrLn $ "Pobieram kursy walut: " ++ unwords currs ++ " z ostatnich 30 dni..."
 
-      ratesList <- fetchRatesInRange manager curr startDay today
+            ratesLists <- mapM (\curr -> fetchRatesInRange manager curr startDay today >>= \rs -> return (curr, rs)) currs
+            let filteredRates = filter (not . null . snd) ratesLists
 
-      let sortedRates = List.sortOn fst ratesList
-          last10 = take 10 $ reverse sortedRates
+            when (null filteredRates) $
+                putStrLn "Nie udało się pobrać żadnych kursów dla podanych walut."
 
-      when (null last10) $ putStrLn "Nie udało się pobrać żadnych kursów."
+            let allPerCurr =
+                    [ (curr, reverse $ List.sortOn fst rs)
+                    | (curr, rs) <- filteredRates
+                    ]
 
-      -- Create plots directory if it doesn't exist
-      createDirectoryIfMissing True "plots"
+            createDirectoryIfMissing True "plots"
 
-      let fileName = "plots" </> (curr ++ "_to_PLN.svg")
+            toFile def fileName $ do
+                layout_title .= "Kursy walut do PLN (ostatnie 10 dni)"
+                layout_x_axis . laxis_title .= "Data"
+                layout_y_axis . laxis_title .= "Kurs"
+                mapM_ (\(curr, points) -> plot (line curr [points])) allPerCurr
 
-      toFile def fileName $ do
-        layout_title .= "Kurs " ++ curr ++ " do PLN (ostatnie 10 dni)"
-        layout_x_axis . laxis_title .= "Data"
-        layout_y_axis . laxis_title .= "Kurs"
-        plot (line (curr ++ "/PLN") [ [ (d, r) | (d, r) <- last10 ] ])
-
-      putStrLn $ "Wykres zapisano do " ++ fileName
-
-    _ -> putStrLn "Podaj walutę jako argument."
+            putStrLn $ "Wykres zapisano do " ++ fileName
